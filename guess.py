bbadcbe50b36d064ec7e3d8889f985dbc69e19b4
Robert Greener <robertgreener1@gmail.com>
2011-12-20 18:31:14 +0000

    guess.py added

--- guess.py
+++ guess.py
@@ -6,8 +6,8 @@ guessesTaken = 0
 print("Hello! What is your name?")
 myName = input()
 
-number = random.randint(1, 20)
-print("Well, " + myName + ", I am thinking of a number between 1 and 20.")
+number = random.randint(1, 100)
+print("Well, " + myName + ", I am thinking of a number between 1 and 100.")
 
 while guessesTaken < 6:
     print("Take a guess.") # There are four spaces in front of print.
