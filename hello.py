a7d5346a48a905e55dc7c3506614d57575622d9b
Robert Greener <robertgreener1@gmail.com>
2011-12-20 17:58:03 +0000

    First Commit

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # This program says hello and asks for my name.
-print("Hello world!")
+print("Hello World!")
 print("What is your name?")
 myName = input()
-print('It is good to meet you, ' + myName)
+print("It is good to meet you, " + myName)
